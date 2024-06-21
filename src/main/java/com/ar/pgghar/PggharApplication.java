9ddb0795e981efd12535da617bea8fcf5aea2e1e
Akash Tiwari <akashtiwari722003@gmail.com>
2024-06-22 02:44:17 +0530

    initial_commit

---
+++ src/main/java/com/ar/pgghar/PggharApplication.java
@@ -0,0 +1,13 @@
+package com.ar.pgghar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PggharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PggharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/pgghar/PggharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.pgghar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PggharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
