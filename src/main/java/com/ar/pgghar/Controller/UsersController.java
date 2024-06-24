a18dcaec03761dceda0e0a543d6bd521fa99fd31
Akash Tiwari <akashtiwari722003@gmail.com>
2024-06-24 15:45:59 +0530

    controller_basic

---
+++ src/main/java/com/ar/pgghar/Controller/UsersController.java
@@ -0,0 +1,22 @@
+package com.ar.pgghar.Controller;
+
+
+import com.ar.pgghar.entity.Users;
+import com.ar.pgghar.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsersController {
+
+    @Autowired
+    UsersService usersService;
+
+    @GetMapping("/getall")
+    public List<Users> getAllUsers(){
+     return usersService.findAll();
+    }
+}
